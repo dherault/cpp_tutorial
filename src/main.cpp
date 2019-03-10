f30aed30e3353b2703f26502da1e67ce05a20b27
David Hérault <dherault@gmail.com>
2019-03-10 15:37:36 +0100

    add functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <math.h>
+
+int getValueFromUser() {
+	int input{ 0 };
+
+  std::cout << "Enter an integer: ";
+	std::cin >> input;  
+ 
+	return input;
+}
+
+int square(int x) {
+  return x *x;
+}
 
 int main() {
   int a; // Variable allocation
+
+  std::cout << a << "\n"; // Undefined behavior
 
   a = 50; // Copy initialization
 
@@ -11,11 +27,24 @@ int main() {
   // << insertion operator
   std::cout << "a + b + c = " << a + b + c << "\n";
 
-  int x;
+  // int x;
   
-  std::cout << "Enter any number: ";
+  // std::cout << "Enter any number: ";
+  
+  // >> extraction operator
+  // std::cin >> x;
 
-  std::cin >> x;
+  // std::cout << x * x << "\n";
 
-  std::cout << x * x << "\n";
+  // Operator precedance: PEMDAS Parenthesis first, then Exponents, then Multiplication & Division, then Addition & Subtraction.
+
+  double d{ pow(2, 10) };
+
+  std::cout << "d = " << d << "\n";
+
+  int value{ getValueFromUser() };
+
+  std::cout << "value² = " << square(value) << "\n";
+
+  return 0;
 }
